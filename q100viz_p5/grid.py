322ab15e20a8dcb7879174d80ecbf4cdb0389dd1
Jan Behrens <36447927+janbehrens@users.noreply.github.com>
2021-05-05 15:32:58 +0200

    fix grid orientation, class GridCell

--- q100viz_p5/grid.py
+++ q100viz_p5/grid.py
@@ -8,8 +8,8 @@ class Grid:
     def __init__(self, size, corners):
         self.x_size, self.y_size = size
 
-        # each grid cell is a tuple representing (ID, rotation)
-        self.grid = [[(-1, -1)] * self.y_size] * self.x_size
+        # initialize two-dimensional array of grid cells
+        self.grid = [[GridCell() for x in range(self.x_size)] for y in range(self.y_size)]
 
         # create the corner pin surface for projection
         self.surface = keystone.CornerPinSurface(corners)
@@ -28,12 +28,12 @@ class Grid:
         with p5.push_style():
             try:
                 for y, row in enumerate(self.grid):
-                    for x, (id, rot) in enumerate(row):
+                    for x, cell in enumerate(row):
                         p5.stroke(stroke)
                         p5.stroke_weight(stroke_weight)
-                        p5.fill(p5.Color(*colors[id]) if id > -1 else p5.Color(0, 0, 0, 0))
+                        p5.fill(p5.Color(*colors[cell.id]) if cell.id > -1 else p5.Color(0, 0, 0, 0))
                         p5.begin_shape()
-                        for _x, _y in [(x, y), (x, y + 1), (x + 1, y + 1), (x + 1, y)]:
+                        for _x, _y in [[x, y], [x, y + 1], [x + 1, y + 1], [x + 1, y]]:
                             p5.vertex(
                                 _x * width / self.x_size,
                                 _y * height / self.y_size
@@ -49,8 +49,15 @@ class Grid:
             print("Invalid JSON")
             return
 
-        # convert flat list to two-dimensional grid
-        self.grid = [array[i:i + self.x_size] for i in range(0, len(array), self.x_size)]
+        # update grid cells
+        try:
+            for y, row in enumerate(self.grid):
+                for x, cell in enumerate(row):
+                    cell.id, cell.rot = array[y * self.y_size + x]
+        except TypeError:
+            pass
+        except IndexError:
+            print("Warning: incoming grid has unexpected size")
 
         # self.print()
 
@@ -58,8 +65,15 @@ class Grid:
         try:
             for row in self.grid:
                 for cell in row:
-                    print("/".join(map(str, cell)), end="\t")
+                    print(f"{cell.id}/{cell.rot}", end="\t")
                 print()
             print()
         except TypeError:
             pass
+
+
+class GridCell:
+    def __init__(self, id=-1, rot=-1):
+        self.id = id
+        self.rot = rot
+        self.selected = False

e9f0398f30693382cea33ebf2fea7590ea438f13
dunland <david.unland@web.de>
2021-08-04 10:42:30 +0200

    edit_mode adjusted to main

--- q100viz/interaction/edit_mode.py
+++ q100viz/interaction/edit_mode.py
@@ -1,4 +1,5 @@
-from pygame.locals import KEYDOWN, K_TAB, K_UP, K_DOWN, K_LEFT, K_RIGHT, K_s
+from pygame.locals import KEYDOWN, K_TAB, K_UP, K_DOWN, K_LEFT, K_RIGHT, K_s, K_MINUS, K_PLUS, K_SPACE
+from pygame import font
 import shapely
 
 import q100viz.session as session
@@ -7,23 +8,29 @@ import q100viz.session as session
 class EditMode:
     def __init__(self):
         self.polygon_selector = 0
+        # session.buildings.iloc[self.polygon_selector, 7] = True  # mark building as selected
         self.relevant_polygons = []
 
     def process_event(self, event, config):
         buildings_file = config['SAVED_BUILDINGS_FILE']
 
-        session.buildings.iloc[self.polygon_selector, 6] = True  # mark building as selected
+        session.buildings.iloc[self.polygon_selector, 7] = True  # mark building as selected
 
         if event.type == KEYDOWN:
             if event.key == K_PLUS:
-                self.polygon_selector = (self.polygon_selector + 1) % (len(session.buildings)-1)
-                session.buildings.iloc[self.polygon_selector, 6] = True  # mark building as selected
-                session.buildings.iloc[self.polygon_selector-1, 6] = False  # unselect previous
+                self.polygon_selector = (self.polygon_selector + 1) % (len(session.buildings))
+                session.buildings.iloc[self.polygon_selector, 7] = True  # mark building as selected
+                session.buildings.iloc[self.polygon_selector-1, 7] = False  # unselect previous
+                print(session.buildings)
+
 
             elif event.key == K_MINUS:
-                self.polygon_selector = (self.polygon_selector - 1) % (len(session.buildings)-1)
-                session.buildings.iloc[self.polygon_selector, 6] = True  # mark building as selected
-                session.buildings.iloc[self.polygon_selector+1, 6] = False  # unselect previous
+                self.polygon_selector = (self.polygon_selector - 1) % (len(session.buildings))
+                session.buildings.iloc[self.polygon_selector, 7] = True  # mark building as selected
+                session.buildings.iloc[(self.polygon_selector + 1) % (len(session.buildings)), 7] = False  # unselect previous
+                if self.polygon_selector == len(session.buildings)-1:
+                    session.buildings.iloc[0, 7] = False  # unselect first when at going to last
+                print(session.buildings)
 
             elif event.key == K_SPACE:
                 self.relevant_polygons.append(session.buildings.index.values[self.polygon_selector])
@@ -46,8 +53,8 @@ class EditMode:
                 session.buildings.iloc[self.polygon_selector, 0] = shapely.geometry.Polygon(points)
             
             elif event.key == K_s:
-                session.buildings['geometry'].to_file(SAVED_BUILDINGS_FILE)
-                print('saved buildings.shp to', SAVED_BUILDINGS_FILE)
+                session.buildings['geometry'].to_file(buildings_file)
+                print('saved buildings.shp to', buildings_file)
                 with open('export/relevant_polygons.txt', "w") as txt_file:
                     for osm_id in self.relevant_polygons:
                         txt_file.write(str(osm_id) + ",")
@@ -60,20 +67,20 @@ class EditMode:
                 session.buildings[session.buildings.selected], 3, (255, 255, 255)
             )
 
-        font = pygame.font.SysFont('Arial', 11, True, False)
+        myFont = font.SysFont('Arial', 11, True, False)
         # mouse_x = pygame.mouse.get_pos()[0]
         # mouse_y = pygame.mouse.get_pos()[1]
         # text = font.render(str(mouse_x)+","+str(mouse_y), True, (255, 255, 255))
-        text = font.render(str(session.buildings.index.values[self.polygon_selector]), True, (255, 255, 255))
+        text = myFont.render(str(session.buildings.index.values[self.polygon_selector]), True, (255, 255, 255))
         session.viewport.blit(text, (1500, 800))
 
         relevant_polygons_string = ""
         for snip in self.relevant_polygons:
             relevant_polygons_string = relevant_polygons_string + ", " + str(snip)
-        text = font.render(relevant_polygons_string, True, (255, 255, 255))
+        text = myFont.render(relevant_polygons_string, True, (255, 255, 255))
         session.viewport.blit(text, (100, 800))
 
         # display num of buildings:
-        text = font.render(str(self.polygon_selector % (len(session.buildings)-1)) + "/" + str(len(session.buildings)-2), True, (255, 255, 255))
+        text = myFont.render(str(self.polygon_selector % (len(session.buildings)-1)) + "/" + str(len(session.buildings)), True, (255, 255, 255))
         session.viewport.blit(text, (1500, 830))
 

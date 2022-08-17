bad727d389a7996888030611e1d250fe0da32a70
dunland <david.unland@web.de>
2022-08-17 15:35:24 +0200

    more plots

--- sandbox/scenario_analysis.py
+++ sandbox/scenario_analysis.py
@@ -11,7 +11,10 @@ print(os.getcwd())
 
 def main():
     '''choose which of the plots to display'''
-    plot_energy_prices()
+    # plot_energy_prices_separated()
+    # plot_carbon_prices()
+    plot_carbon_prices_separated()
+    # plot_q100_prices()
 
 ######################### carbon prices ###############################
 def plot_carbon_prices():
@@ -28,11 +31,36 @@ def plot_carbon_prices():
             ax=plt.gca())
 
     plt.title("Carbon Prices")
+    plt.ylabel("[€/g]")
+    plt.show()
+
+# separate plots:
+def plot_carbon_prices_separated():
+    '''display carbon price plots in separate windows'''
+
+    file = "../data/includes/csv-data_technical/carbon-prices.csv"
+
+    df = pandas.read_csv(file).set_index('year')
+
+    for col in df.columns:
+
+        plt.figure("Carbon Prices {0}".format(col))
+        df.plot(
+            kind='area',
+            y=col,
+            ax=plt.gca(),
+            color='grey'
+        )
+
+        plt.gca().get_legend().remove()
+
+    plt.ylabel("[€/g]")
     plt.show()
 
 ############################# energy prices ###########################
-def plot_energy_prices():
-    '''display energy prices using subplots'''
+# sorted by type:
+def plot_energy_prices_type_sorted():
+    '''display energy prices in subplots, sorted by energy carrier'''
     file = "../data/includes/csv-data_technical/energy_prices-emissions.csv"
 
     df = pandas.read_csv(file)
@@ -56,9 +84,66 @@ def plot_energy_prices():
     for ax in [ax1, ax2, ax3]:
         ax.legend(['scenario_old', 'scenario_2021', 'scenario_2022'])
 
-    fig.canvas.set_window_title('Energy Prices')
+    fig.canvas.set_window_title('Energy Prices (type sorted)')
     ax1.set_ylabel("[ct/kWh]??????????")
-    plt.title("Energy Prices")
+    plt.show()
+
+# sorted by scenario:
+def plot_energy_prices_scenario_sorted():
+    '''display energy prices in subplots, sorted by scenario'''
+    file = "../data/includes/csv-data_technical/energy_prices-emissions.csv"
+
+    df = pandas.read_csv(file)
+    print(df)
+
+    fig, (ax1, ax2, ax3) = plt.subplots(1,3, sharey=True)
+    ax1.plot(df['year'], df['power_prices_scenario_old'],
+             df['year'], df['gas_prices_scenario_old'],
+             df['year'], df['oil_prices_scenario_old'])
+    ax2.plot(df['year'], df['power_prices_scenario_2021'],
+             df['year'], df['gas_prices_scenario_2021'],
+             df['year'], df['oil_prices_scenario_2021'])
+    ax3.plot(df['year'], df['power_prices_scenario_2022'],
+             df['year'], df['gas_prices_scenario_2022'],
+             df['year'], df['oil_prices_scenario_2022'])
+
+    ax1.set_title('scenario_old')
+    ax2.set_title('scenario_2021')
+    ax3.set_title('scenario_2022')
+
+    for ax in [ax1, ax2, ax3]:
+        ax.legend(['power', 'gas', 'oil'])
+
+    fig.canvas.set_window_title('Energy Price (scenario sorted)')
+    ax1.set_ylabel("[ct/kWh]??????????")
+    plt.show()
+
+# no subplots:
+def plot_energy_prices_separated():
+    '''export separate images for each scenario containing power price, gas price, oil price'''
+
+    file = "../data/includes/csv-data_technical/energy_prices-emissions.csv"
+
+    df = pandas.read_csv(file).set_index("year")
+
+    for scenario in ['old', '2021', '2022']:
+        plt.figure("Prices Scenario '{0}'".format(scenario))
+
+        df.plot(
+            kind='line',
+            y=['power_prices_scenario_{0}'.format(scenario),
+                'gas_prices_scenario_{0}'.format(scenario),
+                'oil_prices_scenario_{0}'.format(scenario)],
+            ax=plt.gca(),
+            color=['#EAD41E', '#1E6FEA', '#604741']
+            )
+
+        plt.ylim((0, 70))
+        plt.ylabel("ct/kWh")
+        plt.xlabel("Jahr")
+        plt.legend(['Strom', 'Gas', 'Öl'])
+        plt.title("Energiekosten Szenario '{0}'".format(scenario))
+
     plt.show()
 
 

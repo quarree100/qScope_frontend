a6b4f4f40ac8ec880ba06e75ded571d2734f3f25
dunland <david.unland@web.de>
2022-09-15 11:19:57 +0200

    beautification dataset comparison

--- sandbox/compare_datasets.py
+++ sandbox/compare_datasets.py
@@ -16,15 +16,16 @@ for folder in folder1, folder2:
     for root, dirs, files in os.walk(folder, topdown=False):
         for name in files:
             if name[-4:].__eq__('.csv'):
-                print(os.path.join(root, name))
                 list_of_files[folder_idx].append(os.path.join(root, name))
 
     folder_idx += 1
 
+print("found files: ", list_of_files)
 ############################ compare data #############################
 # connections_df1 = pandas.read_csv(folder1 + '/connections/connections_export.csv')
 # connections_df2 = pandas.read_csv(folder2 + '/connections/connections_export.csv')
 for i in range(len(list_of_files[0])):
+    print("#"*72)
     print("comparing {0} with {1}: ".format(list_of_files[0][i], list_of_files[1][i]))
     if pandas.read_csv(list_of_files[0][i]).compare(pandas.read_csv(list_of_files[1][i])).empty:
         print("both simulations produced the same results!")
